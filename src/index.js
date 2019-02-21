98c46497fb832ef40496b3544082eacc8e90e420
Kosta <satkon@gmail.com>
2019-02-21 12:58:37 +1000

    Write to file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello nodejs')
+const fs = require('fs')
+const path = require('path')
+
+fs.writeFile(path.join(__dirname, 'hello.txt'), 'Hello world!', err => {
+  if (err) {
+    console.log('Error write to file!')
+  }
+})
