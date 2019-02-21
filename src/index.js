efa9762995163eddff10474de1b4c53c97ec4e47
Kosta <satkon@gmail.com>
2019-02-21 13:28:29 +1000

    Fix eslint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
 fs.writeFile(path.join(__dirname, 'hello.txt'), 'Hello world!', err => {
   if (err) {
-    console.log('Error write to file!')
+    console.log('Error write to file!');
   }
-})
+});
