886e7af07124581611e19588b4110a2df99b5128
Kosta <satkon@gmail.com>
2019-02-22 12:07:20 +1000

    delete folder function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
+const fs = require('fs');
 const walk = require('./modules/walkSync');
 
 const sourceFolder = process.argv[2];
+const outputFolder = process.argv[3];
+
+// Checks if the folder exists, if not creates it
+const checkOrCreateFolder = function (dirName) {
+  if (!fs.existsSync(dirName)) {
+    fs.mkdirSync(dirName);
+  }
+};
+
+// Check input params
+if (!sourceFolder || !outputFolder) {
+  throw new Error('Source and destination folder names are required!');
+} else {
+  checkOrCreateFolder(outputFolder);
+}
+
 walk(sourceFolder);
